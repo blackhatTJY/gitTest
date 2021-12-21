e5f7189b9367d91c9aa1b50129e5d07caec377c6
tjy <13574097903@163.com>
2021-12-21 11:05:01 +0800

    1

---
+++ src/main/java/com/example/gittest/Test.java
@@ -0,0 +1,12 @@
+package com.example.gittest;
+
+/**
+ * @author: TangJY
+ * @date: 10:41 2021/12/21
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println(1);
+    }
+}
